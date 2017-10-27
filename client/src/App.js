1a2744a981bf1e34650483bb0090e6fd5868b1fa
szeidman <sam.zeidman@gmail.com>
2017-10-27 15:44:24 -0400

    Continued debugs of Auth0, refactor TopBar into own component

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,13 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch, NavLink } from 'react-router-dom';
-import Charities from './components/Charities';
-import Favorites from './components/Favorites';
-import Home from './components/Home';
+import TopBar from './components/TopBar';
 import './App.css';
-
-const Top = () =>
-  <Router>
-    <div>
-      <NavLink style={{ marginRight: '10px' }} to="/">Home</NavLink>
-      <NavLink style={{ marginRight: '10px' }} to="/charities">Charities</NavLink>
-      <NavLink style={{ marginRight: '10px' }} to="/favorites">Favorites</NavLink>
-      <Switch>
-        <Route exact path="/" compenent={Home} />
-        <Route path="/charities" component={Charities} />
-        <Route path="/favorites" component={Favorites} />
-      </Switch>
-    </div>
-  </Router>
-;
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-        <Top />
+        <TopBar />
       </div>
     );
   }
