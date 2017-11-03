4224704a3276d463248222aea3966a93673fd3ec
szeidman <sam.zeidman@gmail.com>
2017-11-03 13:36:13 -0400

    Use react-router to update active Tab for parent and child components

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Route, Switch, Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Route, Switch, NavLink, withRouter } from 'react-router-dom';
 import Charities from './Charities';
 import Favorites from './Favorites';
 import Home from './Home';
@@ -9,31 +10,62 @@ import ActionHome from 'material-ui/svg-icons/action/home';
 import SocialPublic from 'material-ui/svg-icons/social/public';
 import {cyan100} from 'material-ui/styles/colors';
 
-const Header = () =>
-    <div>
-      <Tabs inkBarStyle={{backgroundColor: cyan100}}>
-       <Tab
-        label="Home"
-        containerElement={<Link to="/"/>}
-        icon={<ActionHome />}
-        />
-       <Tab
-        label="Charities"
-        containerElement={<Link to="/charities"/>}
-        icon={<SocialPublic />}
-       />
-       <Tab
-          label="Favorites"
-          containerElement={<Link to="/favorites"/>}
-          icon={<ActionFavorite />}
-       />
-      </Tabs>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/charities" component={Charities} />
-        <Route path="/favorites" component={Favorites} />
-      </Switch>
-    </div>
-;
+class Header extends Component {
 
-export default Header;
+  componentDidMount(){
+  }
+
+  componentWillReceiveProps(){
+  }
+
+  getTabValue() {
+    const Pathname = this.props.location.pathname;
+    if (Pathname.includes("/charities")) {
+      return "charities"
+    } else if (Pathname.includes("/favorites")){
+      return "favorites"
+    } else {
+      return "home"
+    }
+  }
+
+  render() {
+    const TabValue = this.getTabValue()
+
+    return (
+      <div>
+        <Tabs inkBarStyle={{backgroundColor: cyan100}} value={TabValue}>
+         <Tab
+            label="Home"
+            containerElement={<NavLink to="/"/>}
+            icon={<ActionHome />}
+            value="home"
+          />
+         <Tab
+            label="Charities"
+            containerElement={<NavLink to="/charities"/>}
+            icon={<SocialPublic />}
+            value="charities"
+         />
+         <Tab
+            label="Favorites"
+            containerElement={<NavLink to="/favorites"/>}
+            icon={<ActionFavorite />}
+            value="favorites"
+         />
+        </Tabs>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/charities" component={Charities} />
+          <Route path="/favorites" component={Favorites} />
+        </Switch>
+      </div>
+    )
+  }
+};
+
+const mapStateToProps = (state) => {
+  return {}
+}
+
+export default withRouter(connect(mapStateToProps)(Header));
